952fc16f31bbd753ff0c2fdcaf9bfc64c1b3bd27
Akshay Kamalapuram Sridhar <akkamala@ncsu.edu>
2021-04-10 16:39:44 -0400

    Create README.sh

---
+++ README.sh
@@ -0,0 +1,17 @@
+Each git branch represents a software field and consists of projects in that field.
+
+Can put the overall tree structure of all the project names here and details
+
+1) Back End
+  a) Spring Restapi - 2 line description
+  b) 
+2) Data Science
+  a) Python Webscaping - 2 line description
+3) Front End
+4) Full Stack
+....
+
+In each branch, add a readme to provide the list of all the projects and inside each folder add a readme to describe the project
+
+
+Do similarly for Electronics, Mech, Firmware etc....
