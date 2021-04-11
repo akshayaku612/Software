4d8013a750f855827b6a119a6512df396ab2b508
Akshay Kamalapuram Sridhar <akkamala@ncsu.edu>
2021-04-11 00:33:00 -0400

    Update README.sh

--- README.sh
+++ README.sh
@@ -1,3 +1,5 @@
 For Backend development projects, please refer to "back-end" branch
 
 For Data Science projects, please refer to "data-science" branch
+
+For Automation projects, please refer to "automation" branch
